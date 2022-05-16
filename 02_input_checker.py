4acee3868cbc65561f5a158b093af3ed9bfaebe8
JackPringle <101750172+JackPringle@users.noreply.github.com>
2022-05-16 13:15:15 +1200

    Add files via upload
    End of video 12

---
+++ 02_input_checker.py
@@ -0,0 +1,11 @@
+# Checks user choice is 'integer', 'text' or 'image'
+def user_choice():
+
+    valid = False
+    while not valid:
+
+        response = "File type (integer / text / image): ".lower()
+
+        if response == "text" or response == "t":
+
+            end of video 12
