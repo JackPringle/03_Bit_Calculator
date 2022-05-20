26fe42db360b97a27e22b8203f396c5255bfba02
JackPringle <101750172+JackPringle@users.noreply.github.com>
2022-05-20 14:42:23 +1200

    Add files via upload
    video 12

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -4,8 +4,22 @@ def user_choice():
     valid = False
     while not valid:
 
-        response = "File type (integer / text / image): ".lower()
+        # ask user for choice and change response to lowercase
+        response = input("File type (integer / text / image): ").lower()
 
+        # If they choose "t" or "text", return "text"
         if response == "text" or response == "t":
+            return "text"
 
-            end of video 12
+        else:
+            print("Please choose a valid file type!")
+            print()
+
+
+# Main routine goes here
+data_type = user_choice()
+print("You choose",data_type)
+
+print()
+
+                
