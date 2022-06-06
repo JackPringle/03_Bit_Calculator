8e3e528f803d5868d4537363b787c7e5e03a82f1
JackPringle <101750172+JackPringle@users.noreply.github.com>
2022-06-07 11:55:07 +1200

    End of video 16

--- 05_image_bits.py
+++ 05_image_bits.py
@@ -26,4 +26,24 @@ def num_check(question, low):
 
 # finds # of bits for 24 bit colour
 def image_bits():
-    
+
+    # get width and height...
+    image_width = num_check("Image width? ", 1)
+    image_height = num_check("Image height? ", 1)
+
+    # calculate # of pixels
+    num_pixels = image_width * image_height
+
+    # calculate # of bits (24 x num pixels)
+    num_bits = num_pixels * 24
+
+    # output answer with working
+    print()
+    print("# of pixels = {} x {} x {}".format(image_height, image_width, num,_pixels))                                         
+    print("# bits = {} x 24 = {}").format(num_pixels, num_bits)
+    print()
+
+    return ""
+
+# Main routine goes here
+image_bits()
